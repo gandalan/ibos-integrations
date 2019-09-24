bce29c731eb09816e5acc5c123fbc13404d0eea5
Philipp Reif <philipp.reif@gandalan.de>
2019-09-24 16:07:24 +0200

    Implemented phone call samples

---
+++ OpenAddressByNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace OpenAddressByNumber
+{
+    class Program
+    {
+        /// <summary>
+        /// Opens an IBOS3 address by phone number. The first command line argument 
+        /// will be passed to the IBOS3 remote control endpoint to open the address.
+        /// </summary>
+        /// <param name="args"></param>
+     
+        static void Main(string[] args)
+        {   
+            if (args.Length > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("http://127.0.0.1:15477/adresse-by-phone?msn=");
+                sb.Append(args[0]);
+                var result = new WebClient().DownloadString(sb.ToString());
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TapiIntegrationSample/Program.cs
@@ -0,0 +1,57 @@
+﻿using JulMar.Atapi;
+using System;
+using System.Net;
+using System.Text;
+
+namespace TapiIntegrationSample
+{
+    class Program
+    {
+        /// <summary>
+        /// Opens an IBOS3 address by phone number. The number will be taken from a TAPI 
+        /// event and will be passed to the IBOS3 remote control endpoint to open the address.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            TapiManager tapiManager = new TapiManager("TapiCallMonitor.net");
+            if (tapiManager.Initialize() == false)
+            {
+                Console.WriteLine("No Tapi devices found.");
+                Console.ReadLine();
+            }
+            else
+            {
+                foreach (TapiLine line in tapiManager.Lines)
+                {
+                    try
+                    {
+                        line.NewCall += OnNewCall;
+                        line.Monitor();
+                    }
+                    catch (TapiException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                Console.WriteLine("Waiting for calls");
+                Console.ReadLine();
+                tapiManager.Shutdown();
+            }
+        }
+
+        private static void OnNewCall(object sender, NewCallEventArgs e)
+        {
+            TapiLine line = (TapiLine)sender;
+            TapiCall call = e.Call;
+            Console.WriteLine(call.CallerId);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("http://127.0.0.1:15477/adresse-by-phone?msn=");
+            sb.Append(call.CallerId);
+            var result = new WebClient().DownloadString(sb.ToString());
+            Console.WriteLine(result);
+        }
+    }
+}
